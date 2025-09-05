6b458a1739b30b184849c5e989cb19959d6e590d
Anmol <166452068+Anmol1l@users.noreply.github.com>
2025-09-06 01:59:28 +0530

    Rework code to make pixels dynamic

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ for(i = 1; i <= size * size; i++) {
 
     let square = document.createElement("div")
     square.classList = "square";
+    square.style.height = (968 / size) -2 + 'px'
+    square.style.width = (968 / size) -2 + 'px'
     container.appendChild(square);
 }
 }
 
-function colourOnHover () {
+function colourOnHover() {
 let allSquares = document.querySelectorAll(".square")
 allSquares.forEach(square => square.addEventListener('mouseenter', () => square.style.backgroundColor = "aqua") )
 }
@@ -18,33 +20,23 @@ allSquares.forEach(square => square.addEventListener('mouseenter', () => square.
 grid(16)
 colourOnHover()
 
+gridSize.addEventListener('click', function() { makeNewGrid()})
+
 let size;
-gridSize.addEventListener('click', function() {makeNewGrid() } )
-
 function makeNewGrid () {
-    // remove old grid
-    const removeSquare = document.querySelectorAll(".square")
-    removeSquare.forEach(square => container.removeChild(square))
-    let size = Number(prompt("Enter size of grid: ", "1-100")) 
+    let rmSquare = document.querySelectorAll(".square")
+    rmSquare.forEach(square => square.remove())
 
-    // make new grid if size less than 100
+    size = Number(prompt("Enter a Number: "))
 
-    if (size <= 100) {
-    console.log(size)
+    if (size >=1 && size <= 100) {
     grid(size)
-    let gridWidth = 25 * size
-    container.style.width = gridWidth + "px"
     colourOnHover()
     }
-    // make default grid for invalid value
-    
     else {
-    alert ("enter a valid value")
-    
-    grid(16)
-    container.style.width = "400px"
-    colourOnHover()
-    
+        alert("Enter valid number")
+        grid(16)
+        colourOnHover()
     }
 }
 
